f95b6536b8657b9e7ae1610d2dd5d4dd65e6d061
Armin <armin@coralic.nl>
2017-04-02 10:18:31 +0200

    Working draft

---
+++ README.md
@@ -0,0 +1,29 @@
+# Clair scanner
+
+## Docker containers vulnerability scan
+
+When you work with containers (Docker) you are not only packaging your application but also part of the OS. Therefore it is crucial to know what kind of libraries might be vulnerable in you container. One way to find this information is to use and look at Docker Hub or Quay.io security scan. The problem whit these scans is that they are only showing you the information but are not part of you CI/CD that actually blocks your container when it contains vulnerabilities.
+
+What you want is:
+
+1. To build and test your application
+1. Build the container
+1. Test the container for vulnerabilities
+1. Check the vulnerabilities against allowed ones, if everything is allowed pass, otherwise fail
+
+This straight forward process is not that easy to achieve when using the services like Docker Hub or Quay.io. Docker Hub does not have an API and Quay.io triggers afterwards and therefore makes it harder to do straight forward CI/CD pipeline. (Although it is possible to achieve this with Quay.io because they have a web hook that notifies you about the vulnerabilities).
+
+## Clair to the rescue
+
+CoreOS has created an awesome container scan tool called "clair". Clair is also used by Quay.io. What clair does not have is a simpel tool that scans your image and compares the vulnerabilities against a whitelist to see if they are approved or not.
+
+This is where clair-scanner comes in to place. The clair-scanner does the following:
+
+* Scans an image against Clair server
+* Compares the vulnerabilities against a whitelist
+* Tells you if there are vurnabilities that are not in the whitelist and fails
+* If everything is fine it completes correctly
+
+## Clair server of standalone
+
+For the clair-scanner to work you need a clair server. It is not always convenient to have a dedicated clair server therefore I have created a way to run this standalone. See here https://github.com/arminc/clair-local-scan
---
+++ main.go
@@ -0,0 +1,349 @@
+package main
+
+import (
+	"archive/tar"
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"gopkg.in/yaml.v2"
+
+	"github.com/coreos/clair/api/v1"
+	"github.com/docker/docker/client"
+	"github.com/fatih/color"
+)
+
+const (
+	scriptTerminatedByControlC = 130
+	generalExit                = 0
+	success                    = 0
+	tmpPrefix                  = "clair-scanner-"
+	httpPort                   = 9279
+	postLayerURI               = "/v1/layers"
+	getLayerFeaturesURI        = "/v1/layers/%s?vulnerabilities"
+)
+
+type vulnerabilityInfo struct {
+	vulnerability string
+	namespace     string
+	severity      string
+}
+
+type acceptedVulnerability struct {
+	Cve         string
+	Description string
+}
+
+type projectWhitelist struct {
+	ImageName     string
+	Vurnabilities []acceptedVulnerability
+}
+
+type vulnerabilitiesWhitelist struct {
+	GeneralWhitelist []acceptedVulnerability
+	Projects         []projectWhitelist
+}
+
+func main() {
+	flag.Parse()
+	start(flag.Args()[0], parseWhitelist(flag.Args()[1]), flag.Args()[2], flag.Args()[3])
+	os.Exit(success)
+}
+
+func parseWhitelist(whitelistFile string) vulnerabilitiesWhitelist {
+	whitelist := vulnerabilitiesWhitelist{}
+	whitelistBytes, err := ioutil.ReadFile(whitelistFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = yaml.Unmarshal(whitelistBytes, &whitelist)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	return whitelist
+}
+
+func start(imageName string, whitelist vulnerabilitiesWhitelist, clairURL string, scannerIP string) {
+	tmpPath := createTmpPath()
+	defer os.RemoveAll(tmpPath)
+	interrupt := make(chan os.Signal)
+	signal.Notify(interrupt, os.Interrupt, os.Kill)
+
+	analyzeCh := make(chan error, 1)
+	go func() {
+		analyzeCh <- analyzeImage(imageName, tmpPath, clairURL, scannerIP, whitelist)
+	}()
+
+	select {
+	case <-interrupt:
+		os.Exit(scriptTerminatedByControlC)
+	case err := <-analyzeCh:
+		if err != nil {
+			os.Exit(generalExit)
+		}
+	}
+}
+
+func createTmpPath() string {
+	tmpPath, err := ioutil.TempDir("", tmpPrefix)
+	if err != nil {
+		log.Fatalf("Could not create temporary folder: %s", err)
+	}
+	return tmpPath
+}
+
+func analyzeImage(imageName string, tmpPath string, clairURL string, scannerIP string, whitelist vulnerabilitiesWhitelist) error {
+	err := saveImage(imageName, tmpPath)
+	if err != nil {
+		log.Printf("Could not save the image %s", err)
+		return err
+	}
+	layerIds, err := getImageLayerIds(tmpPath)
+	if err != nil {
+		log.Printf("Could not read the image layer ids %s", err)
+		return err
+	}
+	if err = analyzeLayers(layerIds, tmpPath, clairURL, scannerIP); err != nil {
+		log.Printf("Analyzing faild: %s", err)
+		return err
+	}
+	vulnerabilities, err := getVulnerabilities(clairURL, layerIds)
+	if err != nil {
+		log.Printf("Analyzing failed: %s", err)
+		return err
+	}
+	err = vulnerabilitiesApproved(vulnerabilities, whitelist)
+	if err != nil {
+		log.Printf("Image contains unapproved vulnerabilities: %s", err)
+		return err
+	}
+	return nil
+}
+
+func vulnerabilitiesApproved(vulnerabilities []vulnerabilityInfo, whitelist vulnerabilitiesWhitelist) error {
+	for i := 0; i < len(vulnerabilities); i++ {
+		if vulnerabilityApproved(vulnerabilities[i].vulnerability, whitelist) == false {
+			return errors.New("Vailed on vulnerability")
+		}
+	}
+	return nil
+}
+
+func vulnerabilityApproved(vulnerability string, whitelist vulnerabilitiesWhitelist) bool {
+	for i := 0; i < len(whitelist.GeneralWhitelist); i++ {
+		if vulnerability == whitelist.GeneralWhitelist[i].Cve {
+			return true
+		}
+	}
+	return false
+}
+
+func analyzeLayers(layerIds []string, tmpPath string, clairURL string, scannerIP string) error {
+	ch := make(chan error)
+	go listenHTTP(tmpPath, ch)
+	select {
+	case err := <-ch:
+		return fmt.Errorf("An error occured when starting HTTP server: %s", err)
+	case <-time.After(100 * time.Millisecond):
+		break
+	}
+
+	tmpPath = "http://" + scannerIP + ":" + strconv.Itoa(httpPort)
+	var err error
+
+	for i := 0; i < len(layerIds); i++ {
+		log.Printf("Analyzing %s\n", layerIds[i])
+
+		if i > 0 {
+			err = analyzeLayer(clairURL, tmpPath+"/"+layerIds[i]+"/layer.tar", layerIds[i], layerIds[i-1])
+		} else {
+			err = analyzeLayer(clairURL, tmpPath+"/"+layerIds[i]+"/layer.tar", layerIds[i], "")
+		}
+		if err != nil {
+			return fmt.Errorf("Could not analyze layer: %s", err)
+		}
+	}
+	return nil
+}
+
+func saveImage(imageName string, tmpPath string) error {
+	docker := createDockerClient()
+	imageID := []string{imageName}
+	imageReader, err := docker.ImageSave(context.Background(), imageID)
+	if err != nil {
+		return err
+	}
+
+	defer imageReader.Close()
+	return untar(imageReader, tmpPath)
+}
+
+func createDockerClient() *client.Client {
+	docker, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+	return docker
+}
+
+func getImageLayerIds(path string) ([]string, error) {
+	mf, err := os.Open(path + "/manifest.json")
+	if err != nil {
+		return nil, err
+	}
+	defer mf.Close()
+
+	// https://github.com/docker/docker/blob/master/image/tarexport/tarexport.go#L17
+	type manifestItem struct {
+		Config   string
+		RepoTags []string
+		Layers   []string
+	}
+
+	var manifest []manifestItem
+	if err = json.NewDecoder(mf).Decode(&manifest); err != nil {
+		return nil, err
+	} else if len(manifest) != 1 {
+		return nil, err
+	}
+	var layers []string
+	for _, layer := range manifest[0].Layers {
+		layers = append(layers, strings.TrimSuffix(layer, "/layer.tar"))
+	}
+	return layers, nil
+}
+
+func untar(imageReader io.ReadCloser, target string) error {
+	tarReader := tar.NewReader(imageReader)
+
+	for {
+		header, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		path := filepath.Join(target, header.Name)
+		info := header.FileInfo()
+		if info.IsDir() {
+			if err = os.MkdirAll(path, info.Mode()); err != nil {
+				return err
+			}
+			continue
+		}
+
+		file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, info.Mode())
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		if _, err = io.Copy(file, tarReader); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func listenHTTP(path string, ch chan error) {
+	fileServer := func(path string) http.Handler {
+		fc := func(w http.ResponseWriter, r *http.Request) {
+			http.FileServer(http.Dir(path)).ServeHTTP(w, r)
+			return
+		}
+		return http.HandlerFunc(fc)
+	}
+
+	ch <- http.ListenAndServe(":"+strconv.Itoa(httpPort), fileServer(path))
+}
+
+func analyzeLayer(clairURL, path, layerName, parentLayerName string) error {
+	payload := v1.LayerEnvelope{
+		Layer: &v1.Layer{
+			Name:       layerName,
+			Path:       path,
+			ParentName: parentLayerName,
+			Format:     "Docker",
+		},
+	}
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	request, err := http.NewRequest("POST", clairURL+postLayerURI, bytes.NewBuffer(jsonPayload))
+	if err != nil {
+		return err
+	}
+	request.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != 201 {
+		body, _ := ioutil.ReadAll(response.Body)
+		return fmt.Errorf("Got response %d with message %s", response.StatusCode, string(body))
+	}
+
+	return nil
+}
+func getVulnerabilities(clairURL string, layerIds []string) ([]vulnerabilityInfo, error) {
+	var vulnerabilities = make([]vulnerabilityInfo, 0)
+	//Last layer gives you all the vulnerabilities of all layers
+	rawVulnerabilities, err := fetchLayerVulnerabilities(clairURL, layerIds[len(layerIds)-1])
+	if err != nil {
+		return vulnerabilities, err
+	}
+	if len(rawVulnerabilities.Features) == 0 {
+		fmt.Printf("%s No features have been detected in the image. This usually means that the image isn't supported by Clair.\n", color.YellowString("NOTE:"))
+		return vulnerabilities, nil
+	}
+
+	for _, feature := range rawVulnerabilities.Features {
+		if len(feature.Vulnerabilities) > 0 {
+			for _, vulnerability := range feature.Vulnerabilities {
+				vulnerability := vulnerabilityInfo{vulnerability.Name, vulnerability.NamespaceName, vulnerability.Severity}
+				vulnerabilities = append(vulnerabilities, vulnerability)
+			}
+		}
+	}
+	return vulnerabilities, nil
+}
+
+func fetchLayerVulnerabilities(clairURL string, layerID string) (v1.Layer, error) {
+	response, err := http.Get(clairURL + fmt.Sprintf(getLayerFeaturesURI, layerID))
+	if err != nil {
+		return v1.Layer{}, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		body, _ := ioutil.ReadAll(response.Body)
+		err := fmt.Errorf("Got response %d with message %s", response.StatusCode, string(body))
+		return v1.Layer{}, err
+	}
+
+	var apiResponse v1.LayerEnvelope
+	if err = json.NewDecoder(response.Body).Decode(&apiResponse); err != nil {
+		return v1.Layer{}, err
+	} else if apiResponse.Error != nil {
+		return v1.Layer{}, errors.New(apiResponse.Error.Message)
+	}
+
+	return *apiResponse.Layer, nil
+}
